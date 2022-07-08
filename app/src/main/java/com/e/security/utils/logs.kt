32b56a6e4e3d4abbcae673b73fd2a3ca3475f59e
Netanel <netanel4443@gmail.com>
2022-07-08 17:09:32 +0300

    first commit

--- app/src/main/java/com/e/security/utils/logs.kt
+++ app/src/main/java/com/e/security/utils/logs.kt
@@ -2,22 +2,23 @@ package com.e.security.utils
 
 import android.util.Log
 
-const val DBG=true
-fun printErrorIfDbg(tag:String,message:String?){
-    if (DBG){
-        Log.e(tag,message.toString())
+const val DBG = false
+fun printErrorIfDbg(tag: String, message: String?) {
+    if (DBG) {
+        Log.e(tag, message.toString())
     }
     println()
 
 }
-fun printErrorIfDbg(e:Throwable){
-    if (DBG){
-        Log.e(e.javaClass.name,e.message.toString())
+
+fun printErrorIfDbg(e: Throwable) {
+    if (DBG) {
+        Log.e(e.javaClass.name, e.message.toString())
     }
 }
 
-fun printIfDbg(tag:String,message:String?){
-    if (DBG){
-        Log.i(tag,message.toString())
+fun printIfDbg(tag: String, message: String?) {
+    if (DBG) {
+        Log.i(tag, message.toString())
     }
 }
