4df4c220957a1204596741bb30df9c67402149b0
Netanel <netanel4443@gmail.com>
2022-07-25 14:10:17 +0300

    add extensions to subscribe block

--- app/src/main/java/com/e/safety/utils/rxjavaUtils.kt
+++ app/src/main/java/com/e/safety/utils/rxjavaUtils.kt
@@ -1,6 +1,7 @@
 package com.e.safety.utils
 
 import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.Disposable
@@ -19,7 +20,22 @@ fun <T> Single<T>.subscribeBlock(block: (T) -> Unit): Disposable {
     }, ::printErrorIfDbg)
 }
 
+fun <T> Single<T>.subscribeBlock(block: (T) -> Unit, error:()->Unit): Disposable {
+    return subscribe({
+        block.invoke(it)
+    }, {
+        error()
+        printErrorIfDbg(it)
+    })
+}
+
 fun Completable.subscribeBlock(block: () -> Unit): Disposable {
+    return subscribe({
+        block.invoke()
+    }, ::printErrorIfDbg)
+}
+
+fun <T> Maybe<T>.subscribeBlock(block: () -> Unit): Disposable {
     return subscribe({
         block.invoke()
     }, ::printErrorIfDbg)
